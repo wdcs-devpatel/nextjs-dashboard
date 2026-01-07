b57955a01fb75c0ebae703f5c521cb51f0cd7d9b
wdcs-devpatel <dev.patel@codezeros.com>
2026-01-07 15:49:52 +0530

    Initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import LoginForm from '@/app/ui/login-form';
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen items-center justify-center">
+      <LoginForm />
+    </main>
+  );
+}
